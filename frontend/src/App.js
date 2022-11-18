b497169ccb5aae45a895a3b449983ded75260410
Hackster254 <hackster.origin@gmail.com>
2022-11-18 23:21:05 +0300

    Frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import ProductList from "./components/ProductList";
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import AddProduct from "./components/AddProduct";
+import EditProduct from "./components/EditProduct";
+function App() {
+  return (
+    <div className="container">
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ProductList />} />
+        <Route path="/add" element={<AddProduct />}/>
+        <Route path="/edit/:id" element={<EditProduct />} />
+      </Routes>
+      </BrowserRouter>
+      
+      
+    
+    </div>
+  );
+}
+
+export default App;
